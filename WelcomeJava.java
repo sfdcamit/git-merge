f077901745981c48a4f68a7add0920244ac23caa
sfdcamit <45686346+sfdcamit@users.noreply.github.com>
2018-12-07 15:49:06 +0530

    Added 2 Java Files

---
+++ WelcomeJava.java
@@ -0,0 +1,11 @@
+/*
+  @Author: - Amit Singh
+  @Date: - 4th December
+*/
+public class WelcomeJava{
+    public static void main(String[] agrs){
+        for(int i=10; i>1; i--){
+            System.out.println("The value of i is: "+i);
+         }
+    }
+}
